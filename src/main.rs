7f997dd8fd59ff273f0f313c6978726ea0254b8f
Rickard Dybeck <dybeck@spotify.com>
2019-12-02 19:59:55 -0500

    day 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod day1;
+mod day2;
 
 fn main() {
+    println!("\n\n###### Day 1 : Fuel Check");
     day1::total_fuel();
+
+    println!("\n\n###### Day 2 : Gravity Assist");
+    day2::gravity_assist();
 }
