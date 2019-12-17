81f30a2293814eea771a0bb1d1d8bddae14b3d80
alde <r.dybeck@gmail.com>
2019-12-16 22:12:59 -0500

    day 6 part 2

--- golang/day6/day6.go
+++ golang/day6/day6.go
@@ -13,6 +13,8 @@ func main() {
 	}
 	planets := parse(strings.TrimSpace(string(input)))
 	fmt.Printf("total orbits: %d\n", planets.countAllOrbits())
+	
+	fmt.Printf("orbital transfers: %d\n", planets.getDistance("YOU", "SAN"))
 }
 
 func parse(input string) Planets {
@@ -46,10 +48,55 @@ func (p Planets) countOrbits(name string) int {
 	}
 	return planet.Orbits
 }
+
 func (p Planets) countAllOrbits() int {
 	count := 0
 	for name := range p {
 		count += p.countOrbits(name)
 	}
 	return count
+}
+
+func (p Planets) getPath(from string) []string {
+	path := []string{}
+	planet, ok := p[from]
+	for ok {
+		path = append(path, planet.Name)
+		planet, ok = p[planet.Name]
+	}
+	return path
+}
+
+func (p Planets) getDistance(from string, to string) int {
+	pathFrom := p.getPath(from)
+	pathTo := p.getPath(to)
+	path := travelFrom(pathFrom, pathTo, []string{})
+	return len(path)
+}
+
+func travelFrom(pathFrom []string, pathTo []string, path []string) []string {
+	planet := pathFrom[0]
+	tail := pathFrom[1:]
+	idx := index(pathTo, planet)
+	if idx != -1 {
+		return travelTo(pathTo, idx, path)
+	}
+	return travelFrom(tail, pathTo, append(path, planet))
+}
+
+func travelTo(pathTo []string, index int, path []string) []string {
+	if index == 0 {
+		return path
+	}
+	planet := pathTo[index]
+	return travelTo(pathTo, index - 1, append(path, planet))
+}
+
+func index(s []string, e string) int {
+    for i, a := range s {
+        if a == e {
+            return i
+        }
+    }
+    return -1
 }
--- golang/day6/day6_test.go
+++ golang/day6/day6_test.go
@@ -1,13 +1,51 @@
 package main
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
-	  "github.com/stretchr/testify/assert"
 )
 
 func Test_countAllOrbits(t *testing.T) {
 	input := "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L"
 	planets := parse(input)
 	actual := planets.countAllOrbits()
-	assert.Equal(t, 42, actual)
+	assert.Equal(t, actual, 42)
+}
+
+func Test_getPath(t *testing.T) {
+	testData := []struct {
+		in  string
+		out []string
+	}{
+		{"C", []string{"B", "COM"}},
+		{"YOU", []string{"K", "J", "E", "D", "C", "B", "COM"}},
+	}
+	input := "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN"
+	planets := parse(input)
+	for _, td := range testData {
+		t.Run(td.in, func(t *testing.T) {
+			path := planets.getPath(td.in)
+			assert.Equal(t, path, td.out)
+		})
+	}
+}
+
+func Test_getDistance(t *testing.T) {
+	testData := []struct {
+		from     string
+		to       string
+		distance int
+	}{
+		{"L", "H", 6},
+		{"YOU", "SAN", 4},
+	}
+	input := "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN"
+	planets := parse(input)
+	for _, td := range testData {
+		t.Run(fmt.Sprintf("distance from %s to %s", td.from, td.to), func(t *testing.T) {
+			distance := planets.getDistance(td.from, td.to)
+			assert.Equal(t, distance, td.distance)
+		})
+	}
 }
