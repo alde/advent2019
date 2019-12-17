4af0faa2fe69f49624d6276db1c56a59488cabcc
alde <r.dybeck@gmail.com>
2019-12-16 20:09:41 -0500

    day 5 part 1

--- golang/lib/opcode.go
+++ golang/lib/opcode.go
@@ -1,33 +1,90 @@
 package lib
+
 import (
 	"fmt"
 )
+
 // ResetCode resets the input code
 func ResetCode(input []int, noun int, verb int) ([]int, error) {
 	input[0] = noun
 	input[1] = verb
-	return OpCode(input)
+
+	return OpCode(input, func() (i int) {
+			fmt.Scan(&i)
+			return i
+		})
 }
 
 // OpCode runs the computer
-func OpCode(input []int) ([]int, error) {
-	return record(input, 0)
+func OpCode(input []int, handler func() int) ([]int, error) {
+	return record(input, handler, 0)
 }
 
-func record(input []int, offset int) ([]int, error) {
+func record(input []int, handler func() int, offset int) ([]int, error) {
 	if input[offset] == 99 {
 		return input, nil
 	}
-	a := input[offset+1]
-	b := input[offset+2]
-	c := input[offset+3]
-	switch input[offset] {
+	var stepping int
+	i := input[offset]
+	modeSet, operation := extractOpcode(i)
+
+	switch operation {
 	case 1:
-		input[c] = input[a] + input[b]
+		stepping = opcodeOne(input, offset, modeSet)
 	case 2:
-		input[c] = input[a] * input[b]
+		stepping = opcodeTwo(input, offset, modeSet)
+	case 3:
+		stepping = opcodeThree(input, offset, modeSet, handler)
+	case 4:
+		stepping = opcodeFour(input, offset, modeSet)
 	default:
 		return nil, fmt.Errorf("unknown operation %d", input[offset])
 	}
-	return record(input, offset+4)
+	return record(input, handler, offset+stepping)
+}
+
+func modeSet(i int, index int, memo []int) []int {
+	if index == -1 {
+		return memo
+	}
+	memo[index] = i % 10
+	return modeSet(i / 10, index - 1, memo)
+}
+
+func extractOpcode(i int) ([]int, int) {
+	return modeSet(i / 100, 2, []int{0,0,0}), i % 100
+}
+func val(input []int, offset int, modeSet int) int {
+	if modeSet == 0 {
+		return input[input[offset]]
+	}
+	return input[offset]
+}
+func opcodeOne(input []int, offset int,  modeSet []int) int {
+	a := val(input, offset+1, modeSet[2])
+	b := val(input, offset+2, modeSet[1])
+	c := input[offset+3]
+	input[c] = a + b
+	return 4
+}
+
+func opcodeTwo(input []int, offset int,  modeSet []int) int {
+	a := val(input, offset+1, modeSet[2])
+	b := val(input, offset+2, modeSet[1])
+	c := input[offset+3]
+	input[c] = a * b
+	return 4
+}
+
+func opcodeThree(input []int, offset int,  modeSet []int, handler func() int)  int {
+	i := handler()
+	target := input[offset+1]
+	input[target] = i
+	return 2
+}
+
+func opcodeFour(input []int, offset int,  modeSet []int)  int {
+	target := input[offset+1]
+	fmt.Printf("%d\n", input[target])
+	return 2
 }
--- golang/lib/opcode_test.go
+++ golang/lib/opcode_test.go
@@ -1,4 +1,5 @@
 package lib
+
 import (
 	"fmt"
 	"testing"
@@ -6,23 +7,49 @@ import (
 
 func Test_OpCode(t *testing.T) {
 	testData := []struct {
-		in []int
+		in  []int
 		out []int
-	} {
-		{ []int{1,0,0,0,99}, []int{2,0,0,0,99} },
-		{ []int{2,3,0,3,99}, []int{2,3,0,6,99} },
-		{ []int{2,4,4,5,99,0}, []int{2,4,4,5,99,9801} },
-		{ []int{1,1,1,4,99,5,6,0,99}, []int{30,1,1,4,2,5,6,0,99} },
+	}{
+		{[]int{1, 0, 0, 0, 99}, []int{2, 0, 0, 0, 99}},
+		{[]int{2, 3, 0, 3, 99}, []int{2, 3, 0, 6, 99}},
+		{[]int{2, 4, 4, 5, 99, 0}, []int{2, 4, 4, 5, 99, 9801}},
+		{[]int{1, 1, 1, 4, 99, 5, 6, 0, 99}, []int{30, 1, 1, 4, 2, 5, 6, 0, 99}},
+		{[]int{1002, 4, 3, 4, 33}, []int{1002, 4, 3, 4, 99}},
+		{[]int{1101, 100, -1, 4, 0}, []int{1101, 100, -1, 4, 99}},
 	}
 	for _, td := range testData {
 		t.Run(fmt.Sprintf("%+v", td.in), func(t *testing.T) {
-			actual, _ := OpCode(td.in)
+			actual, _ := OpCode(td.in, func() int {
+				return 1
+			})
 			validate(t, actual, td.out)
 		})
 	}
 }
 
+func Test_extractOpcode(t *testing.T) {
+	testData := []struct {
+		in     int
+		mode   []int
+		opcode int
+	}{
+		{2, []int{0, 0, 0}, 2},
+		{1002, []int{0, 1, 0}, 2},
+	}
+	for _, td := range testData {
+		t.Run(fmt.Sprintf("%+v", td.in), func(t *testing.T) {
+			mode, code := extractOpcode(td.in)
+			validate(t, mode, td.mode)
+			if code != td.opcode {
+				fmt.Printf("expected: %+v\nactual: %+v\n", td.opcode, code)
+				t.Fail()
+			}
+		})
+	}
+}
+
 func validate(t *testing.T, actual []int, expected []int) {
+	t.Logf("validating %+v == %+v\n", expected, actual)
 	for idx, e := range actual {
 		if expected[idx] != e {
 			fmt.Printf("expected: %+v\nactual: %+v\n", expected, actual)
