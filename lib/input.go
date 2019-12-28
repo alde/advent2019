5aa5f0f6ef6377ac8b7dc9254c5d29292cb156aa
Rickard Dybeck <dybeck@spotify.com>
2019-12-28 21:01:35 +0100

    add input helper

--- lib/input.go
+++ lib/input.go
@@ -2,6 +2,7 @@ package lib
 
 import (
 	"io/ioutil"
+	"strconv"
 	"strings"
 
 	"github.com/sirupsen/logrus"
@@ -14,4 +15,15 @@ func ReadFile(filename string) string {
 		logrus.WithError(err).WithField("path", filename).Fatal("unable to read file")
 	}
 	return strings.TrimSpace(string(bytes))
+}
+
+// ReadFileAsIntSlice does what you'd think
+func ReadFileAsIntSlice(filename string) []int {
+	s := ReadFile(filename)
+	memo := []int{}
+	for _, c := range strings.Split(s, ",") {
+		i, _ := strconv.Atoi(c)
+		memo = append(memo, i)
+	}
+	return memo
 }
